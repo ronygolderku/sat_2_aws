e12d061ebfebc0bb275e4dd3f8a23660a74a6c5b
MD RONY GOLDER <ronygolderku@gmail.com>
2024-08-10 23:43:13 +1000

    Update ghrsst.py

--- ghrsst.py
+++ ghrsst.py
@@ -18,15 +18,15 @@ s3_client = session.client('s3', endpoint_url='https://projects.pawsey.org.au')
 bucket_name = 'wamsi-westport-project-1-1'
 s3_folder = 'csiem-data/data-lake/NASA/GHRSST/NC/'
 
-# Define the URL for the dataset
-url = "https://polarwatch.noaa.gov/erddap/griddap/jplMURSST41"
+# # Define the URL for the dataset
+# url = "https://polarwatch.noaa.gov/erddap/griddap/jplMURSST41"
 
-# Open the dataset and select the 'analysed_sst' variable with the desired spatial slice
-ds = xr.open_dataset(url)["analysed_sst"].sel(
-    latitude=slice(-33, -31), 
-    longitude=slice(114, 116)
-)
-
+# # Open the dataset and select the 'analysed_sst' variable with the desired spatial slice
+# ds = xr.open_dataset(url)["analysed_sst"].sel(
+#     latitude=slice(-33, -31), 
+#     longitude=slice(114, 116)
+# )
+ds=xr.open_dataset('https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41', engine='netcdf4')
 # Extract the last available date from the dataset
 last_date = ds.time[-1].values
 
