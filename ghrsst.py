bed08d2f536ef84b9a3a44e30970b83b897e31dd
MD RONY GOLDER <ronygolderku@gmail.com>
2024-08-10 22:38:46 +1000

    Update ghrsst.py

--- ghrsst.py
+++ ghrsst.py
@@ -1,4 +1,22 @@
+import os
+import numpy as np
 import xarray as xr
+import boto3
+from botocore.exceptions import ClientError
+
+# Retrieve credentials from environment variables
+aws_access_key = os.getenv("AWS_ACCESS_KEY_ID")
+aws_secret_key = os.getenv("AWS_SECRET_ACCESS_KEY")
+
+# Initialize a Boto3 session
+session = boto3.Session()
+
+# Create an S3 client with a custom endpoint
+s3_client = session.client('s3', endpoint_url='https://projects.pawsey.org.au')
+
+# S3 bucket and folder configuration
+bucket_name = 'wamsi-westport-project-1-1'
+s3_folder = 'csiem-data/data-lake/NASA/GHRSST/NC/'
 
 # Define the URL for the dataset
 url = "https://coastwatch.pfeg.noaa.gov/erddap/griddap/jplMURSST41"
@@ -23,3 +41,14 @@ file_name = f'ghrsst_sst_{time_str}.nc'
 
 # Save the sliced data to a netCDF file
 ds_slice.to_netcdf(file_name)
+
+
+# Upload the netCDF file to the S3 bucket
+try:
+    s3_client.upload_file(file_name, bucket_name, os.path.join(s3_folder, file_name))
+    print(f"File '{file_name}' uploaded to S3 bucket '{bucket_name}' in folder '{s3_folder}'.")
+except Exception as e:
+    print(f"Error uploading file '{file_name}' to S3: {e}")
+
+# Clean up: Delete the local netCDF file
+os.remove(file_name)
